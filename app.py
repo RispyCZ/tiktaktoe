95b3d7886ac52302fa958be4e44771672bdacca9
Petr Václavek <petadj88@gmail.com>
2022-01-13 22:39:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 def vypis_hraci_pole(hraci_pole : list):
     radek = ""
     for i,x in enumerate(hraci_pole):
-        if((i+1) % 3 == 0):
+        if (i+1) % 3 == 0:
             radek += " " + x + "\n"
         else:
            radek += " " + x
@@ -11,9 +11,9 @@ def vypis_hraci_pole(hraci_pole : list):
 def main():
     hraci_pole = ["*", "*", "*", "*", "*", "*","*", "*", "*"]
     pocet = 0
+    vyhra = [[1,2,3], [4,5,6], [7,8,9], [7,4,1], [8,5,2], [9,6,3]]
     hrac = ""
     hraci = ["X", "O"]
-    
     while True:
         # Výběr strany
         while hrac not in hraci:
@@ -25,13 +25,18 @@ def main():
                 hraci_pole[tah] = hrac
                 break
 
+        # Validace výhry
+        for w in vyhra:
+            if(hraci_pole[w[0]-1] == hrac and hraci_pole[w[1]-1] == hrac and hraci_pole[w[2]-1] == hrac):
+                print(f"{hrac} win! Congrats!")
+                break
+
         # Prohození stran
         if(hrac == "X"):
             hrac = "O"
         else: 
             hrac = "X"
         
-
         vypis_hraci_pole(hraci_pole)
 
 if __name__ == "__main__":
